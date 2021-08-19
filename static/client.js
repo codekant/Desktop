96dd586c401e39f911f87fd44560734b014b935c
カント <53932812+itsjustkant@users.noreply.github.com>
2021-08-19 12:02:42 +0530

    weewoo

--- static/client.js
+++ static/client.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    document.body.style.zoom = "100%";
     setTimeout(() => {
         $("#loading").fadeOut(500, function() {
             $("#bgv").trigger("play");
@@ -12,4 +13,13 @@ window.onload = function() {
         let fullDate = `${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}-${((date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1))}-${date.getFullYear()}`;
         $("#time").html(`${time}<br>${fullDate}`);
     };
+    $("#taskbar").on("click", (e) => {
+        if($(".startmenu").css("bottom") == "56px") 
+                return $(".startmenu").css("bottom", "-40rem");
+    })
+    $("#start").on("click", (e) => {
+        $(".startmenu").css("bottom") == "56px" ? 
+        $(".startmenu").css("bottom", "-40rem") : 
+        $(".startmenu").css("bottom", "3.5rem");
+    });
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [ './index.html', 
+    './static/client.js', 
+    './static.css' ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
